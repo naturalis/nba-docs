7d3474092a6b9dff33c251f7860836c6cbcc94b7
Wilfred Gerritsen <wilfred.gerritsen@naturalis.nl>
2016-01-14 17:40:55 +0100

    Update conf.py
    syntax error fixed

--- conf.py
+++ conf.py
@@ -182,7 +182,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'NetherlandsBiodiversityDataServicesc'
+htmlhelp_basename = 'NetherlandsBiodiversityDataServices'
 
 
 # -- Options for LaTeX output --------------------------------------------------
