d88555430dec5f65b734a844d345f2e3923b438d
Wilfred Gerritsen <wilfred.gerritsen@naturalis.nl>
2016-01-14 16:54:40 +0100

    Update make.bat
    remove reference documentation

--- make.bat
+++ make.bat
@@ -99,9 +99,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\NetherlandsBiodiversityDataServicesdocumentation.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\NetherlandsBiodiversityDataServices.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\NetherlandsBiodiversityDataServicesdocumentation.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\NetherlandsBiodiversityDataServices.ghc
 	goto end
 )
 
